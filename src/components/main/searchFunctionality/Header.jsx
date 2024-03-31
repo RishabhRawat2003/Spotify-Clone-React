a30d1d1c6a1c65ff2543c38fac88cd55088e62ea
RishabhRawat2003 <rawatrishabh953@gmail.com>
2024-03-31 20:33:23 +0530

    sidebar done almost

--- src/components/main/searchFunctionality/Header.jsx
+++ src/components/main/searchFunctionality/Header.jsx
@@ -4,17 +4,17 @@ import { RxCross2 } from "react-icons/rx";
 import { FaRegBell } from 'react-icons/fa';
 function Header() {
     return (
-        <header className='w-full h-16 rounded-t-md flex justify-between items-center'>
+        <header className='w-full h-16 rounded-t-md flex justify-between items-center bg-[#121212]'>
             <span className='group ml-2 h-10 w-64 flex justify-center items-center bg-gray-900 rounded-full hover:border-white hover:border-[1px] sm:ml-5 sm:h-12 sm:w-72 lg:w-80'>
                 <IoSearch size={20} className='text-gray-500 group-hover:text-white rounded-full' />
                 <input type="text" placeholder='What do you want to listen?' className='rounded-md h-9 w-48 bg-gray-900 text-sm px-2 outline-none text-white sm:w-52 lg:text-lg lg:w-64' />
                 <RxCross2 size={20} className='text-gray-500 group-hover:text-white rounded-full cursor-pointer' />
             </span>
-            <div className='w-auto h-full flex justify-center items-center gap-2 mr-2'>
-                <div className='h-9 w-9 rounded-full bg-black flex justify-center items-center cursor-pointer hover:scale-105 duration-300 sm:h-11 sm:w-11'>
+            <div className='w-auto h-full flex justify-center items-center gap-2 mr-2 md:mr-3'>
+                <div className='h-9 w-9 rounded-full bg-black flex justify-center items-center cursor-pointer active:scale-105 md:hover:scale-105 md:duration-300 sm:h-11 sm:w-11'>
                     <FaRegBell className='mx-1 text-white stroke-white' size={20} />
                 </div>
-                <div className='h-9 w-9 rounded-full bg-black flex justify-center items-center cursor-pointer hover:scale-105 duration-300 sm:h-11 sm:w-11'>
+                <div className='h-9 w-9 rounded-full bg-black flex justify-center items-center cursor-pointer active:scale-105 md:hover:scale-105 md:duration-300 sm:h-11 sm:w-11'>
                     <span className='w-6 h-6 border-[1px] border-gray-400 rounded-full flex justify-center items-center sm:h-8 sm:w-8'>U</span>
                 </div>
             </div>
--- src/components/sidebar/Library.jsx
+++ src/components/sidebar/Library.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { MdOutlineLibraryMusic } from "react-icons/md";
 import { IoIosSearch } from "react-icons/io";
+import { TiPin } from "react-icons/ti";
+import apiFunc from '../../apifunctions/Apifunction'
+import { Link } from 'react-router-dom';
 
 function Library() {
+    const [data , setData] = useState([])
     const toggle = useSelector((state) => state.sidebar.sidebarStatus)
+
+    useEffect(() => {
+        let token = apiFunc.getToken()
+        token.then((val)=>{
+            let sidebarArtists = apiFunc.getNavArtists(val)
+            sidebarArtists.then((val)=>{
+                let artistList = val.artists
+                setData(artistList)
+            })
+        })
+    }, [])
+
+
+
     return (
-        <div className={toggle ? 'bg-[#121212] h-[334px] w-[170px] mx-auto mt-1 rounded-lg flex flex-col justify-start items-center border-[1px] border-slate-500 gap-2' : 'bg-[#121212] h-full w-16 mx-auto mt-1 rounded-lg flex flex-col justify-start items-center border-[1px] border-slate-500 gap-2 md:border-none md:w-full'}>
+        <div className={toggle ? 'bg-[#121212] h-[334px] w-[170px] mx-auto mt-1 rounded-lg flex flex-col justify-start items-center border-[1px] border-slate-500 gap-2 overflow-y-scroll' : 'bg-[#121212] h-full w-16 mx-auto mt-1 rounded-lg flex flex-col justify-start items-center border-[1px] border-slate-500 overflow-y-scroll gap-2 md:border-none md:w-full'}>
             <div className={toggle ? 'group w-full h-10 my-1 flex justify-start items-center' : 'group w-full h-10 my-1 flex justify-start items-center'}>
-                <MdOutlineLibraryMusic className={toggle ? 'text-gray-400 stroke-gray-400 cursor-pointer ml-3 mr-2 group-hover:text-white' : 'text-gray-400 stroke-gray-400 cursor-pointer group-hover:text-white mx-5'} size={30} />
+                <MdOutlineLibraryMusic className={toggle ? 'text-gray-400 stroke-gray-400 cursor-pointer ml-3 mr-2 group-hover:text-white' : 'text-gray-400 stroke-gray-400 cursor-pointer group-hover:text-white mx-5 mt-2'} size={30} />
                 <span className='text-gray-400 font-semibold hidden cursor-pointer group-hover:text-white text-xl md:block'>Your Library</span>
                 <span className='text-gray-400 font-semibold hidden cursor-pointer hover:text-white text-3xl md:block ml-4'>+</span>
                 {
@@ -22,11 +40,51 @@ function Library() {
                     </div>
                     : <div className='my-2 flex w-full justify-center md:justify-start'>
                         <label htmlFor="library">
-                        <IoIosSearch size={30} className='mx-1 text-gray-500 cursor-pointer hover:text-white' />
+                            <IoIosSearch size={30} className='mx-1 text-gray-500 cursor-pointer hover:text-white' />
                         </label>
                         <input type="text" id='library' placeholder='Search in Your Library' className='w-[80%] bg-slate-900 hidden md:block text-white text-sm px-2 md:text-base outline-none focus:outline-white outline-[1px]' />
                     </div>
-
+            }
+            {
+                toggle
+                    ? <div className='w-full flex h-10 cursor-pointer' >
+                        <img src="https://misc.scdn.co/liked-songs/liked-songs-64.png" alt="img" className='object-cover mx-2 rounded-md' />
+                        <div className='h-full w-auto flex flex-col'>
+                            <p className='text-white font-semibold text-sm'>Liked Songs</p>
+                            <p className='text-gray-500 flex text-xs gap-0.5 items-center'><TiPin size={20} className='text-green-500' /> Playlist</p>
+                        </div>
+                    </div>
+                    : <div className='w-full flex h-12 justify-center cursor-pointer mb-3 md:justify-start md:h-14' >
+                        <img src="https://misc.scdn.co/liked-songs/liked-songs-64.png" alt="img" className='object-cover mx-2 mr-3 rounded-md' />
+                        <div className='h-full w-auto hidden md:flex md:flex-col'>
+                            <p className='text-white font-semibold text-lg'>Liked Songs</p>
+                            <p className='text-gray-500 flex text-sm gap-0.5 items-center'><TiPin size={30} className='text-green-500' /> Playlist</p>
+                        </div>
+                    </div>
+            }
+            {
+                toggle
+                ? data && data.length > 1 
+                ? data.map((items)=>(
+                    <Link key={items.id} className='w-full h-10 my-4 rounded-full flex justify-start items-center'>
+                        <img src={items.images[0].url} alt="img" className='object-cover w-14 h-14 rounded-full'/>
+                        <div className='w-auto h-full mx-2 flex flex-col'>
+                            <p className='text-xs text-white font-semibold'>{items.name}</p>
+                            <p className='text-xs text-gray-500 font-semibold'>{items.type.toUpperCase()}</p>
+                        </div>
+                    </Link>
+                )): <div>Loading</div>
+                : data && data.length > 1 
+                ? data.map((items)=>(
+                    <Link key={items.id} className='w-full h-10 my-4 rounded-full flex justify-center items-center md:justify-start'>
+                        <img src={items.images[0].url} alt="img" className='object-cover w-16 h-16 rounded-full'/>
+                        <div className='w-auto h-full mx-2 hidden md:flex md:flex-col'>
+                            <p className='text-base text-white font-semibold'>{items.name}</p>
+                            <p className='text-sm text-gray-500 font-semibold'>{items.type.toUpperCase()}</p>
+                        </div>
+                    </Link>
+                ))
+                : <div>Loading</div>
             }
         </div>
     )
